2456ab78c6ad8c489b607767809e1158d83da2cc
이승후 <ron222@naver.com>
2022-02-15 04:28:02 +0900

    myNetflix commit

--- MyNetflix/MyNetflix/HomeViewController.swift
+++ MyNetflix/MyNetflix/HomeViewController.swift
@@ -41,6 +41,25 @@ class HomeViewController: UIViewController {
     
 
     @IBAction func playButtonTapped(_ sender: Any) {
+        // 인터스텔라에 대한 정보를 검색 API로 가져온다.
+        // 거기서 인터스텔라 객체 하나를 가져온다.
+        // 그 객체를 이용해서 PlayerViewController를 띄워준다.
+        
+        SearchAPI.search("interstella") { movies in
+            guard let interstella = movies.first else { return }
+            DispatchQueue.main.async {
+                let url = URL(string: interstella.previewURL)!
+                let item = AVPlayerItem(url: url)
+                
+                let sb = UIStoryboard(name: "Player", bundle: nil)
+                let vc = sb.instantiateViewController(identifier: "PlayerViewController") as! PlayerViewController
+                vc.modalPresentationStyle = .fullScreen
+                vc.player.replaceCurrentItem(with: item)
+                self.present(vc, animated: false, completion: nil)
+            
+            }
+        }
+        
 //        SearchAPI.search("interstella") { movies in
 //            guard let interstella = movies.first else { return }
 //            DispatchQueue.main.async {
