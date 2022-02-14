2f721c50417dd95fa803be9a88d66d0ab30c3dbf
이승후 <ron222@naver.com>
2022-02-14 18:15:38 +0900

    myNetflix playerView commit

--- MyNetflix/MyNetflix/PlayerViewController.swift
+++ MyNetflix/MyNetflix/PlayerViewController.swift
@@ -7,8 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlayerViewController: UIViewController {
+    
+    
+    @IBOutlet weak var controlView: UIView!
+    @IBOutlet weak var playerView: PlayerView!
+    @IBOutlet weak var playButton: UIButton!
+    
+    let player = AVPlayer()
     
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask{
         return .landscapeRight
@@ -18,15 +26,13 @@ class PlayerViewController: UIViewController {
         super.viewDidLoad()
     }
     
-
+    @IBAction func togglePlayButton(_ sender: Any) {
+        playButton.isSelected = !playButton.isSelected
+    }
+    
+    
     @IBAction func closeButtonTapped(_ sender: Any) {
         dismiss(animated: false, completion: nil)
     }
 }
 
-//extension AVPlayer {
-//    var isPlaying: Bool {
-//        guard self.currentItem != nil else { return false }
-//        return self.rate != 0
-//    }
-//}
