f922f42890ad9a72a766edee5f67785bbdc6480e
이승후 <ron222@naver.com>
2022-02-18 03:51:46 +0900

    Firebase101 commit

--- Firebase101/Firebase101/ViewController.swift
+++ Firebase101/Firebase101/ViewController.swift
@@ -6,14 +6,24 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var dataLabel: UILabel!
+    let db = Database.database().reference()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        db.child("firstData").observeSingleEvent(of: .value){ snapshot in
+            print("--> \(snapshot)")
+            
+            let value = snapshot.value as? String ?? ""
+            
+            self.dataLabel.text = value
+        }
     }
-
-
 }
 
