b6f1138bdb8ac9f6e09d2237ec21d9e7ed45c6b9
이승후 <ron222@naver.com>
2022-03-03 23:38:54 +0900

    myNetflix App searchHistory update commit

--- MyNetflix/MyNetflix/HistoryViewController.swift
+++ MyNetflix/MyNetflix/HistoryViewController.swift
@@ -9,30 +9,60 @@
 import UIKit
 import Firebase
 
+
 class HistoryViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
     let db = Database.database().reference().child("searchHistory")
-     
+    var searchTerms: [SearchTerm] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
         db.observeSingleEvent(of: .value) { (snapshot) in
-            print("---> snapshot: \(snapshot.value)")
+            
+            guard let searchHistory = snapshot.value  as? [String: Any] else { return }
+            let data =  try! JSONSerialization.data(withJSONObject: Array(searchHistory.values), options: [])
+            let decoder = JSONDecoder()
+            let searchTerms = try! decoder.decode([SearchTerm].self, from: data)
+            // 정렬하는 법
+            self.searchTerms = searchTerms.sorted { $0.timestamp > $1.timestamp }
+            self.tableView.reloadData()
+            print(" ---> snapshot: \(data), \(searchTerms), ")
             
         }
-        
     }
 }
 
-
+extension HistoryViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchTerms.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryCell", for: indexPath) as? HistoryCell
+        else{
+            return UITableViewCell()
+        }
+        cell.searchTerm.text = searchTerms[indexPath.row].term
+        return cell
+    }
+}
 
 class HistoryCell: UITableViewCell {
     @IBOutlet weak var searchTerm: UILabel!
+}
+
+struct SearchTerm: Codable {
+    let term: String
+    let timestamp: TimeInterval
+    
 }
